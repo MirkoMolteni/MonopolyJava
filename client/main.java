b1066d449e12a9cef40ebad4787ec759977052ed
Giacomo Felletti <g.felletti@icloud.com>
2023-11-11 11:40:40 +0100

    Creato client per la comunicazione TCP

--- client/main.java
+++ client/main.java
@@ -1,7 +1,13 @@
 package client;
 
+import java.io.IOException;
+
 public class main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        netUtil net = new netUtil();
+        net.connect("172.22.182.242", 8080);
+        net.send("hi");
+        System.out.println(net.receive());
+        net.close();
         }
 }
---
+++ client/netUtil.java
@@ -0,0 +1,32 @@
+package client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class netUtil {
+    private Socket socket;
+    private BufferedReader in;
+    private PrintWriter out;
+
+    public void connect(String host, int port) throws IOException {
+        socket = new Socket(host, port);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream(), true);
+    }
+
+    public void send(String message) {
+        out.println(message);
+    }
+
+    public String receive() throws IOException {
+        return in.readLine();
+    }
+
+    public void close() throws IOException {
+        socket.close();
+    }
+}
+
