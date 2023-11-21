e442d1317924ba9bf237253b84790b082c4d22ca
Mirko Molteni <mirko.molteni.2005@gmail.com>
2023-11-21 11:31:03 +0100

    Comunicazione tcp ristabilita

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -15,16 +15,15 @@ public class Server {
     public void start() throws Exception {
         Partita p = new Partita();
         while (true) {
-            // Socket clientSocket = serverSocket.accept();
-            // System.out.println("Connessione stabilita con " +
-            // clientSocket.getInetAddress());
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Connessione stabilita con " +
+                    clientSocket.getInetAddress());
 
-            // BufferedReader in = new BufferedReader(new
-            // InputStreamReader(clientSocket.getInputStream()));
-            // PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            Scanner in = new Scanner(System.in);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            // Scanner in = new Scanner(System.in);
             String inputLine;
-            while ((inputLine = in.nextLine()) != null) {
+            while ((inputLine = in.readLine()) != null) {
                 System.out.println("Message received: " + inputLine);
                 String campi[] = inputLine.split(";");
                 String risposta = "";
@@ -60,13 +59,13 @@ public class Server {
                 }
                 System.out.println("Message sent: " + risposta);
                 // invio risposta
-                // out.println(risposta);
+                out.println(risposta);
             }
-            // clientSocket.close();
-            // System.out.println("Connessione con " + clientSocket.getInetAddress() +
-            // "terminata.");
+            clientSocket.close();
+            System.out.println("Connessione con " + clientSocket.getInetAddress() +
+                    "terminata.");
             in.close();
-            // out.close();
+            out.close();
         }
     }
 }
