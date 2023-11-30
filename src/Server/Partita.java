637e7691b92516e6a4f9ad78d392b506cb74e375
Mirko Molteni <mirko.molteni.2005@gmail.com>
2023-11-30 21:24:10 +0100

    Aggiunto id del player che parte al metodo start

--- src/Server/Partita.java
+++ src/Server/Partita.java
@@ -49,7 +49,7 @@ public class Partita {
      */
     public String startGame() {
         Settings.GAME_STATUS = 0;
-        String s = "START";
+        String s = "START;P#1";
 
         for (Entry<String, Player> entry : giocatori.entrySet()) {
             // String key = entry.getKey();
